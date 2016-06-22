5ebbf6aee1e530158d9e22082b818fb6753c3ba2
johnnadratowski <john@unifiedsocial.com>
2016-06-22 08:57:42 -0400

    Added rollback test

--- tx_test.go
+++ tx_test.go
@@ -3,6 +3,7 @@ package golangNeo4jBoltDriver
 import (
 	"testing"
 	"github.com/johnnadratowski/golang-neo4j-bolt-driver/structures/graph"
+	"io"
 )
 
 func TestBoltTx_Commit(t *testing.T) {
@@ -83,6 +84,72 @@ func TestBoltTx_Commit(t *testing.T) {
 	_, err = stmt.ExecNeo(nil)
 	if err != nil {
 		t.Fatalf("An error occurred on delete query to Neo: %s", err)
+	}
+
+	err = conn.Close()
+	if err != nil {
+		t.Fatalf("Error closing connection: %s", err)
+	}
+}
+
+func TestBoltTx_Rollback(t *testing.T) {
+	driver := NewDriver()
+	conn, err := driver.OpenNeo(neo4jConnStr)
+	if err != nil {
+		t.Fatalf("An error occurred opening conn: %s", err)
+	}
+
+	tx, err := conn.Begin()
+	if err != nil {
+		t.Fatalf("An error occurred beginning transaction: %s", err)
+	}
+
+	stmt, err := conn.PrepareNeo(`CREATE (f:FOO {a: "1"})-[b:TO]->(c:BAR)<-[d:FROM]-(e:BAZ) RETURN f, b, c, d, e`)
+	if err != nil {
+		t.Fatalf("An error occurred preparing statement: %s", err)
+	}
+
+	result, err := stmt.ExecNeo(nil)
+	if err != nil {
+		t.Fatalf("An error occurred querying Neo: %s", err)
+	}
+
+	if num, err := result.RowsAffected(); num != 5 {
+		t.Fatalf("Expected 5 rows affected: %#v err: %#v", result.Metadata(), err)
+	}
+
+	err = tx.Rollback()
+	if err != nil {
+		t.Fatalf("An error occurred committing transaction: %s", err)
+	}
+
+	err = stmt.Close()
+	if err != nil {
+		t.Fatalf("An error occurred closing statement")
+	}
+
+	stmt, err = conn.PrepareNeo(`MATCH (f:FOO {a: "1"})-[b:TO]->(c:BAR)<-[d:FROM]-(e:BAZ) RETURN f, b, c, d, e`)
+	if err != nil {
+		t.Fatalf("An error occurred preparing statement: %s", err)
+	}
+
+	rows, err := stmt.QueryNeo(nil)
+	if err != nil {
+		t.Fatalf("An error occurred querying Neo: %s", err)
+	}
+
+	output, _, err := rows.NextNeo()
+	if err != io.EOF {
+		t.Fatalf("Unexpected error returned from getting next rows: %s", err)
+	}
+
+	if len(output) != 0 {
+		t.Fatalf("Unexpected return data: %s", err)
+	}
+
+	err = stmt.Close()
+	if err != nil {
+		t.Fatalf("An error occurred closing statement")
 	}
 
 	err = conn.Close()
